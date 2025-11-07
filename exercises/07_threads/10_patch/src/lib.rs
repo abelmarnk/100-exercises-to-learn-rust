e43fbf8f63f977883da220061e2e5f8e12cb0151
abelmarnk <151375781+abelmarnk@users.noreply.github.com>
2025-11-07 19:42:36 +0100

    Fix private interface warning in exercise 07-10

--- exercises/07_threads/10_patch/src/lib.rs
+++ exercises/07_threads/10_patch/src/lib.rs
@@ -63,7 +63,7 @@ enum Command {
     },
 }
 
-pub fn server(receiver: Receiver<Command>) {
+fn server(receiver: Receiver<Command>) {
     let mut store = TicketStore::new();
     loop {
         match receiver.recv() {
